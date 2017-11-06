02bfed786ec87cea2cdb56562667e9a92bf6789c
Caroline Reid <carolinehreid@gmail.com>
2017-11-06 22:13:47 +0000

    adds hashmap to Words.java

--- app/src/main/java/com/example/carolinereid/wordcount/Words.java
+++ app/src/main/java/com/example/carolinereid/wordcount/Words.java
@@ -1,6 +1,10 @@
 package com.example.carolinereid.wordcount;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by carolinereid on 06/11/2017.
@@ -26,5 +30,31 @@ public class Words {
             this.words.add(word);
         }
         return this.words.size();
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> words = new HashMap<String, Integer>();
+
+        words.put("it", 6);
+        words.put("was", 6);
+        words.put("the", 6);
+        words.put("best", 1);
+        words.put("of", 6);
+        words.put("times", 2);
+        words.put("worst", 1);
+        words.put("age", 2);
+        words.put("wisdom", 1);
+        words.put("foolishness", 1);
+        words.put("epoch", 2);
+        words.put("belief", 1);
+        words.put("incredulity", 1);
+
+        Set set = words.entrySet();
+        Iterator iterator = set.iterator();
+        while(iterator.hasNext()) {
+            Map.Entry mentry = (Map.Entry)iterator.next();
+            System.out.print("the word " + mentry.getKey() + " occurs ");
+            System.out.println(mentry.getValue() + " time(s) in the sentence");
+        }
     }
 }
